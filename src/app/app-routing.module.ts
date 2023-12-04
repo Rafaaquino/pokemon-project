ebdbc51163d89da34ff30c680ce78a1986dc26f7
Rafaaquino <rafaaquino28@gmail.com>
2023-12-04 18:09:06 -0300

    initial: configuracao do projeto e arquitetura

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,22 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule, Routes } from "@angular/router";
+
+const routes: Routes = [
+
+]
+
+@NgModule({
+  declarations: [
+  ],
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,14 @@
+const host = 'https://api.pokemontcg.io'
+
+export const environment = {
+  production: false,
+  api_key: '60677531-3ea8-46c6-8d0f-def7d7ff7be6',
+  host: host,
+  host_suffix: host + '/v2',
+  host_suffix_cards: host + '/cards',
+  host_suffix_sets: host + '/sets',
+  host_suffix_types: host + '/types',
+  host_suffix_subtypes: host + '/subtypes',
+  host_suffix_supertypes: host + '/supertypes',
+  host_suffix_rarities: host + '/rarities',
+};
